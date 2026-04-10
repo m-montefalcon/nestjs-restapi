cd3bb7a52b96a70ed7e03e4574d3dfb4fba25fe7
m-montefalcon <mmontefalconschlprps@gmail.com>
2026-04-10 12:42:13 +0800

    Fix: Initialized dependencies testing modules

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthController', () => {
     let controller: AuthController;
@@ -8,7 +11,35 @@ describe('AuthController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [AuthController],
-            providers: [AuthService],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: {
+                        register: jest.fn(),
+                        login: jest.fn(),
+                        logout: jest.fn(),
+                    },
+                },
+                {
+                    provide: UsersService,
+                    useValue: {
+                        createWithHashedPassword: jest.fn(),
+                        findByEmail: jest.fn(),
+                    },
+                },
+                {
+                    provide: JwtService,
+                    useValue: {
+                        sign: jest.fn(),
+                    },
+                },
+                {
+                    provide: ConfigService,
+                    useValue: {
+                        get: jest.fn(),
+                    },
+                },
+            ],
         }).compile();
 
         controller = module.get<AuthController>(AuthController);
